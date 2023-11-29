551357522a41ae429b2b49514dfffe2e6f7afffe
kiralin <highfoot908@livemail.tw>
2023-11-29 12:39:58 +0800

    Initial Commit

---
+++ Lion King Fantasy Tour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lion King Fantasy Tour
+//
+//  Created by 林郁琦 on 2023/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
